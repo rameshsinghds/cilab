7360b330c67c605c89bb2a10883a3c7435173094
student <ramesh.singh1986@gmail.com>
2021-03-17 09:43:49 +0000

    cilab code base 1st commit

---
+++ src/main/java/ci/lab/cilab/CilabApplication.java
@@ -0,0 +1,13 @@
+package ci.lab.cilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CilabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CilabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ci/lab/cilab/CilabApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.lab.cilab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CilabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
